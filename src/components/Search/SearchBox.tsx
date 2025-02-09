9da940234b1349fe89f350210f6ee57be5ed7ba9
Piyush <piyushschoughule@gmail.com>
2025-02-09 13:32:08 +0200

    better designed search box

--- src/components/Search/SearchBox.tsx
+++ src/components/Search/SearchBox.tsx
@@ -1,19 +1,18 @@
+import CrossIcon from '@mui/icons-material/Clear';
 import { Button, Grid2 as Grid, Input } from '@mui/material';
 import { ChangeEvent, useState } from 'react';
 
 type Props = {
     placeholder?: string;
     searchButtonText?: string;
-    clearButtonText?: string;
     onClick: (searchValue: string) => void;
 }
 
 const SearchBox = ({
     placeholder = 'Type here...',
     searchButtonText = 'Search',
-    clearButtonText = 'Clear',
     onClick }: Props) => {
-    const [searchText, setSearchText] = useState('')
+    const [searchText, setSearchText] = useState('');
 
     return (
         <Grid container alignItems="end" spacing={2}>
@@ -23,25 +22,26 @@ const SearchBox = ({
                     size={"medium"}
                     value={searchText}
                     placeholder={placeholder}
+                    endAdornment={
+                        <Button
+                            size='large'
+                            variant='text'
+                            color='inherit'
+                            onClick={() => { setSearchText(''); onClick('') }}
+                        >
+                            <CrossIcon />
+                        </Button>
+                    }
                     onChange={(e: ChangeEvent<HTMLInputElement>) => setSearchText(e.currentTarget.value)}
                 />
             </Grid>
             <Grid>
                 <Button
+                    size="large"
                     variant="contained"
                     onClick={() => onClick(searchText)}
-                    size="large"
                 >
                     {searchButtonText}
-                </Button>
-            </Grid>
-            <Grid>
-                <Button
-                    size="large"
-                    variant="text"
-                    onClick={() => { setSearchText(''); onClick('') }}
-                >
-                    {clearButtonText}
                 </Button>
             </Grid>
         </Grid >
